20acf41f259ddc5a786811f015c8f0e3e2590744
qgustavor <qgustavor@users.noreply.github.com>
2021-03-31 09:22:40 -0300

    Update stats

--- planning/main.js
+++ planning/main.js
@@ -80,6 +80,9 @@ const app = new Vue({
         return JSON.parse(cachedData)
       }
 
+      // disabled
+      return
+
       const response = await fetch('https://jsonbox.io/qgustavor_anime_stats/5f9dc9b1abd5b40017a9ac47')
       if (!response.ok) {
         if (cachedData) return JSON.parse(cachedData)
